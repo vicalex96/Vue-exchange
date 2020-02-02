5758bd77eb158449bb6051c6310d6ddcbea8d6bf
vicalex96 <vadjdn@gmail.com>
2020-02-02 11:27:06 -0400

    Cargado los archivos del proyecto
    proyecto creado con la ayuda de un tutoria del platzi, sobre como utilizar la herramienta de Vue.js, creando un apñicacion web que permite ver el precio de varias criptomonedas

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- la siguiente etiqueta es para vue spinners --->
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100"/>
+    <px-assets-table v-show="!isLoading" v-bind:assets="assets" />
+  </div>
+</template>
+<script>
+import PxAssetsTable from '@/components/PxAssetsTable'
+import api from '@/api'
+export default {
+  name: 'Home',
+  components: { PxAssetsTable },
+
+  data() {
+    return {
+      assets: [],
+      isLoading: false
+    }
+  },
+
+  created() {
+    this.isLoading = true
+
+    api.getAssets().then(assets => (this.assets = assets))
+    .finally(()=> this.isLoading = false)
+  }
+}
+</script>
